47a4830ed02ebacee4084741dd54a26521971357
floreselisa <132858627+floreselisa@users.noreply.github.com>
2024-11-14 11:47:59 -0800

    Create Astronomy_block.js

---
+++ stimuli/Astronomy_block.js
@@ -0,0 +1,182 @@
+var Astronomy_block = [
+    {
+        "Category": "Astronomy",
+        "Concept": "Planet",
+        "T1": "T",
+        "T2": "T",
+        "Statement": "Planets are more massive than moons.",
+        "M proportion correct": 0.83,
+        "M response time (ms)": 3943
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Planet",
+        "T1": "F",
+        "T2": "F",
+        "Statement": "Asteroids are more massive than planets.",
+        "M proportion correct": 0.79,
+        "M response time (ms)": 3913
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Planet",
+        "T1": "T",
+        "T2": "F",
+        "Statement": "Planets are more massive than stars.",
+        "M proportion correct": 0.64,
+        "M response time (ms)": 3129
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Planet",
+        "T1": "F",
+        "T2": "T",
+        "Statement": "Stars are more massive than planets.",
+        "M proportion correct": 0.61,
+        "M response time (ms)": 3726
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Star",
+        "T1": "T",
+        "T2": "T",
+        "Statement": "The sun produces light.",
+        "M proportion correct": 0.99,
+        "M response time (ms)": 2546
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Star",
+        "T1": "F",
+        "T2": "F",
+        "Statement": "The sun produces sound.",
+        "M proportion correct": 0.84,
+        "M response time (ms)": 2906
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Star",
+        "T1": "T",
+        "T2": "F",
+        "Statement": "The moon produces light.",
+        "M proportion correct": 0.65,
+        "M response time (ms)": 3111
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Star",
+        "T1": "F",
+        "T2": "T",
+        "Statement": "The sun produces gravity.",
+        "M proportion correct": 0.54,
+        "M response time (ms)": 3348
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Solar system",
+        "T1": "T",
+        "T2": "T",
+        "Statement": "The moon revolves around the earth.",
+        "M proportion correct": 0.95,
+        "M response time (ms)": 3121
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Solar system",
+        "T1": "F",
+        "T2": "F",
+        "Statement": "The sun revolves around the moon.",
+        "M proportion correct": 0.94,
+        "M response time (ms)": 3271
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Solar system",
+        "T1": "T",
+        "T2": "F",
+        "Statement": "The sun revolves around the earth.",
+        "M proportion correct": 0.86,
+        "M response time (ms)": 3889
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Solar system",
+        "T1": "F",
+        "T2": "T",
+        "Statement": "The earth revolves around the sun.",
+        "M proportion correct": 0.83,
+        "M response time (ms)": 2948
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Lunar phase",
+        "T1": "T",
+        "T2": "T",
+        "Statement": "Phases of the moon are caused by changes in illumination.",
+        "M proportion correct": 0.95,
+        "M response time (ms)": 3576
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Lunar phase",
+        "T1": "F",
+        "T2": "F",
+        "Statement": "Phases of the moon are caused by clouds.",
+        "M proportion correct": 0.93,
+        "M response time (ms)": 3527
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Lunar phase",
+        "T1": "T",
+        "T2": "F",
+        "Statement": "Phases of the moon are caused by the earth's shadow.",
+        "M proportion correct": 0.39,
+        "M response time (ms)": 5285
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Lunar phase",
+        "T1": "F",
+        "T2": "T",
+        "Statement": "Phases of the moon are caused by the moon's orbit.",
+        "M proportion correct": 0.68,
+        "M response time (ms)": 4784
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Season",
+        "T1": "T",
+        "T2": "T",
+        "Statement": "A change in overall sunlight causes the seasons.",
+        "M proportion correct": 0.95,
+        "M response time (ms)": 3855
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Season",
+        "T1": "F",
+        "T2": "F",
+        "Statement": "The earth's rotation causes the seasons.",
+        "M proportion correct": 0.67,
+        "M response time (ms)": 4901
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Season",
+        "T1": "T",
+        "T2": "F",
+        "Statement": "The earth's distance from the sun causes the seasons.",
+        "M proportion correct": 0.49,
+        "M response time (ms)": 4457
+      },
+      {
+        "Category": "Astronomy",
+        "Concept": "Season",
+        "T1": "F",
+        "T2": "T",
+        "Statement": "The earth's tilt causes the seasons.",
+        "M proportion correct": 0.75,
+        "M response time (ms)": 3590
+      },
+    ];
